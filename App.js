c8bbbf6dfc35caa869bb3757f6abfa996b05edf1
sisoalbert <65389496+sisoalbert@users.noreply.github.com>
2022-02-08 02:06:36 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  const inputRef = React.createRef();
+
+  setTimeout(() => inputRef.current.focus(), 100);
+  return (
+    <View style={{ paddingVertical: 20 }}>
+      <Text>circle</Text>
+      <TextInput ref={inputRef} />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
